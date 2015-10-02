481f412e3278254bb4ee7ff3bda60d68dbc1fd79
masterUNG <phrombutr@gmail.com>
2015-10-02 13:01:29 +0700

    ListView Successful

--- app/src/main/java/appewtc/masterung/prayer/ListviewActivity.java
+++ app/src/main/java/appewtc/masterung/prayer/ListviewActivity.java
@@ -3,18 +3,25 @@ package appewtc.masterung.prayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListviewActivity extends AppCompatActivity {
 
     //Explicit
-    private int indexAnInt;
+    private int indexAnInt, iconAnInt;
     private String[] title1Strings, title2Strings, title3Strings,
             content1Strings, content2Strings, content3Strings;
+    private TextView titleTextView;
+    private ListView contentListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
+
+        //Bind Widget
+        bindWidget();
 
         //Receive From Intent
         receiveFromIntent();
@@ -24,11 +31,20 @@ public class ListviewActivity extends AppCompatActivity {
 
     }   // onCreate
 
+    private void bindWidget() {
+        titleTextView = (TextView) findViewById(R.id.txtShowTitle);
+        contentListView = (ListView) findViewById(R.id.listView);
+    }
+
     private void createListView() {
 
         //Get Title from xml
         title1Strings = getResources().getStringArray(R.array.contentltitle);
         Log.d("Prayer", "Title ==> " + Integer.toString(title1Strings.length));
+
+        //Inherited MyAdapter
+        MyAdapter objMyAdapter = new MyAdapter(ListviewActivity.this, iconAnInt, title1Strings);
+        contentListView.setAdapter(objMyAdapter);
 
     }   // createListView
 
@@ -36,6 +52,29 @@ public class ListviewActivity extends AppCompatActivity {
 
         indexAnInt = getIntent().getIntExtra("index", 0);
         Log.d("Prayer", "index ==> " + Integer.toString(indexAnInt));
+
+        //Show Title
+        String strTitle = null;
+        switch (indexAnInt) {
+            case 0:
+                strTitle = getResources().getString(R.string.content1);
+                iconAnInt = R.drawable.content1;
+                break;
+            case 1:
+                strTitle = getResources().getString(R.string.content2);
+                iconAnInt = R.drawable.content2;
+                break;
+            case 2:
+                strTitle = getResources().getString(R.string.content3);
+                iconAnInt = R.drawable.content3;
+                break;
+            default:
+                strTitle = "Nothing";
+                iconAnInt = R.drawable.content1;
+                break;
+        }   // switch
+
+        titleTextView.setText(strTitle);
 
     }   // receiveFromIntent
 
---
+++ app/src/main/java/appewtc/masterung/prayer/MyAdapter.java
@@ -0,0 +1,59 @@
+package appewtc.masterung.prayer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by masterUNG on 10/2/15 AD.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private int iconAnInt;
+    private String[] titleStrings;
+
+    public MyAdapter(Context objContext, int iconAnInt, String[] titleStrings) {
+        this.objContext = objContext;
+        this.iconAnInt = iconAnInt;
+        this.titleStrings = titleStrings;
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.content_listview, viewGroup, false);
+
+        //Setup Icon
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imvListIcon);
+        iconImageView.setImageResource(iconAnInt);
+
+        //Setup Title
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.txtListTitle);
+        titleTextView.setText(titleStrings[i]);
+
+        return objView1;
+    }   // getView
+
+}   // Main Class
